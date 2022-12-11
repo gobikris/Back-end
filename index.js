ab09cb58f7001a8914cc159c15e20264b06c06bc
gobikris <96045276+gobikris@users.noreply.github.com>
2022-12-11 14:06:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ const app = express();
 
         // middleware 
         
-        app.use(cors({
-            origin:"https://master.d2jydgk3z8ckdr.amplifyapp.com/"
-        }
-            ));
+        app.use(cors());
         app.use(express.json({limit:"50mb"})); 
         app.use(middleware.logging);
         app.use(middleware.maintenance);
